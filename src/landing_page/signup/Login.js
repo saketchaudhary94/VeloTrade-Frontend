6a0ea6166c1eb6b78d2541139beca4aa1ccf0b37
saket singh <saketchaudhary2004@gmail.com>
2025-05-29 00:41:03 +0530

    frontend last commit

--- src/landing_page/signup/Login.js
+++ src/landing_page/signup/Login.js
@@ -31,7 +31,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/login",
+        "https://vetotrade-backend-6.onrender.com/login",
         {
           ...inputValue,
         },
--- src/landing_page/signup/signup.js
+++ src/landing_page/signup/signup.js
@@ -33,7 +33,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/signup",
+        "https://vetotrade-backend-6.onrender.com/signup",
         {
           ...inputValue,
         },
