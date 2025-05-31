56b390579fd86ab6256843a3ddf6ffb18a47f767
saket singh <saketchaudhary2004@gmail.com>
2025-05-31 16:16:06 +0530

    d

--- src/landing_page/home/Education.js
+++ src/landing_page/home/Education.js
@@ -10,9 +10,9 @@ function Education() {
                 <div className='col'>
                     <h2>Free and open market education</h2>
                     <p>Varsity, the largest online stock market education book in the world <br></br>covering everything from the basic to advance trading.</p>
-                    <a style={{textDecoration : "none"}} href='#'>Versity <i className="fa-solid fa-arrow-right-long"></i></a>
+                    <a style={{textDecoration : "none"}} href="javascript:void(0)">Versity <i className="fa-solid fa-arrow-right-long"></i></a>
                     <p className='mt-5'>TradingQ&A the most active trading and investment community in  <br></br>India for all your market related queries.</p>
-                    <a style={{textDecoration : "none"}} href='#'>TradingQ&A <i className="fa-solid fa-arrow-right-long"></i></a>
+                    <a style={{textDecoration : "none"}} href="javascript:void(0)">TradingQ&A <i className="fa-solid fa-arrow-right-long"></i></a>
                 </div>
             </div>
         </div> 
