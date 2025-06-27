a1301bfa39bfa604b7d5f93e399beccef6f0ab98
saket singh <saketchaudhary2004@gmail.com>
2025-06-27 19:23:44 +0530

    same

--- src/landing_page/products/ProductPage.js
+++ src/landing_page/products/ProductPage.js
@@ -13,7 +13,7 @@ function ProductPage() {
         <LeftSection imageUrl="media/images/Coin.png" productName="Coin" productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices." tryDemo="" learnMore="" googlePlay="" appStore="" />
         <RightSection imageUrl="media/images/kiteconnect.png" productName="Kite Connect API" productDescription="Build powerful trading platforms and experiences with our super simple HTTP/JSON APIs. If you are a startup, build your investment app and showcase it to our clientbase." learnMore="" />
         <LeftSection imageUrl="media/images/varsity.png" productName="Varsity mobile" productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go." tryDemo="" learnMore="" googlePlay="" appStore="" />
-        <p className='text-center fs-5 mt-5'>Want to know more about our technology stack? Check out the <a style={{textDecoration : "none"}} href=''>VeloTrade.tech</a> blog.</p>
+        <p className='text-center fs-5 mt-5'>Want to know more about our technology stack? Check out the VeloTrade.tech blog.</p>
         <Universe />
         </>
     );
